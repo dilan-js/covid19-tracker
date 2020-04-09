e3e6280ac02d82e49394ba210e74eed2120bc8b7
Dilan  Nana <dilannana.js@gmail.com>
2020-04-08 23:58:51 -0500

    Updates

---
+++ precache-manifest.ffe9bedad5386ff19bdb205c3e9d7bd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a30c83a51c237f15c10db50d91c4ef",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "3a509aec34d2f14e042f",
+    "url": "/covid19-tracker/static/css/main.cd41f36e.chunk.css"
+  },
+  {
+    "revision": "454d80f05120a3f19c98",
+    "url": "/covid19-tracker/static/js/2.065429b2.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid19-tracker/static/js/2.065429b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a509aec34d2f14e042f",
+    "url": "/covid19-tracker/static/js/main.b872276b.chunk.js"
+  },
+  {
+    "revision": "55daa2417f757b783985",
+    "url": "/covid19-tracker/static/js/runtime-main.c309573c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
